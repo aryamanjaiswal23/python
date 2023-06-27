8222f9b3d26b69a23333dfd02f8b1e3dd6607c49
tylerdurden158 <aryamanjaiswal23@gmail.com>
2023-06-27 15:22:26 +0530

    intro

---
+++ intro.py
@@ -0,0 +1,45 @@
+#getting started
+print("hello world")    
+
+# data types : iinteger, float , boolean and complex are represented over here
+t=2
+tr=2.132435
+ty=complex(3,4)
+print(t,tr,ty)
+r=12+(-3.23)
+print(r)
+ttt=False
+print(ttt)
+
+#strings and their operations
+s="nre string for testing"
+print(s)
+
+print(len(s),s[len(s)-3],s[-4],s[3:10],s[10:2:-1])
+
+print(id(s))
+s=""
+print(id(s))
+
+# arithimatic opertaions
+
+print(32//3,32/3,3*3.3) 
+print(3*4/4*3,(3*4)/(4*5))
+n1=10
+n2=2
+n3=2.2345
+print(n1>n2,n1+n2<n3)
+print(n1-8 is n2)
+n2*=5
+print(n1==n2)
+print(10*True,10*False)
+
+num1=10
+num2 =20
+print(num1 & num2)
+print(num1 | num2)
+print(num1>>3)
+print(~num1)
+print('djsl' in s)
+print("hello "* 3)
+print(2**3)
